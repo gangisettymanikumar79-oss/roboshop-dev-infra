161ca37c219fbc799baf0d16f5c434b2c7343615
.email.com <gangisettymani98@gmail.com>
2026-08-29 16:56:34 +0530

    if i want create new file

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,7 +2,7 @@
 component=$1
 enviroment=$2
 dnf install ansible -y
-mkdir -P /var/log/roboshop/
+mkdir -p /var/log/roboshop/
 chown -R ec2-user:ec2-user /var/log/roboshop/
 chmod -R 755 /var/log/roboshop/
 touch /var/log/roboshop/ansible.log
