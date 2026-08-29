bf563e7083a016c4f7860880008d0ff86225c4e4
.email.com <gangisettymani98@gmail.com>
2026-08-29 13:02:27 +0530

    if i want create new file

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -12,3 +12,4 @@ git clone  https://github.com/gangisettymanikumar79-oss/roboshop-ansible-V3.git
 cd roboshop-ansible-v3
 git pull
 ansible-playbook -e component=$component -e env=$environment roboshop.yaml
+
