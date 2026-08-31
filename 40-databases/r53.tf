92a4c08e1f679a46f83cab80c5ae4d2b7d8d7368
.email.com <gangisettymani98@gmail.com>
2026-08-31 17:11:40 +0530

    message

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -17,3 +17,11 @@ resource "aws_route53_record" "redis" {
 }
 
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
+}
