eb182ff3bcb445b9e6026b4eea81444d654c0866
.email.com <gangisettymani98@gmail.com>
2026-09-01 12:08:03 +0530

    message

---
+++ 40-databases/iamrole.tf
@@ -0,0 +1,49 @@
+resource "aws_iam_role" "mysql" {
+  name = "${local.common_name}-mysql"
+
+# This is the trust policy, means we can attach this role to EC2 instance
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    {
+        Name = "${local.common_name}-mysql"
+    },
+    local.common_tags
+  )
+}
+
+
+resource "aws_iam_policy" "mysql" {
+  name        = "local.common_name"
+  description =  "Policy to read MySQL SSM paramter to attach to mysql instance"
+
+   # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode
+}
+
+resource "aws_iam_role_policy_attachment" "mysql" {
+  role       = aws_iam_role.mysql.name
+  policy_arn = aws_iam_policy.mysql.arn
+}
+
+resource "aws_iam_role_policy_attachment" "mysql" {
+  role       = "${local.common_name}-mysql"
+  policy_arn = aws_iam_policy.mysql.arn
+}
+  
+  
+
+ 
