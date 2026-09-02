63f48d14e3defc3b92f7269c210afb641631dd35
.email.com <gangisettymani98@gmail.com>
2026-09-02 16:24:31 +0530

    if i want create new file

--- 60-catalogue/local.tf
+++ 60-catalogue/local.tf
@@ -3,7 +3,7 @@ locals {
     backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
     ami_id = data.aws_ami.joindevops.id
     common_name  = "${var.project}-${var.environment}"
-    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_id.value)[0]
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
     common_tags = {
     project     = var.project
     environment = var.environment
