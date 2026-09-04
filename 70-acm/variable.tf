ade993ed02dcbfd7dc3591a17dbbd1df09f70a41
.email.com <gangisettymani98@gmail.com>
2026-09-04 20:27:50 +0530

    if i need edit new code

---
+++ 70-acm/variable.tf
@@ -0,0 +1,20 @@
+variable "domain_name" {
+    default = "manikumar.online"
+  
+}
+
+variable "project" {
+    default = "roboshop"
+
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+ variable "zone_id" {
+    default = "Z04044114R8X5UCWP928"
+  
+} 
+
+ 
