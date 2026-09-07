5f44257cd16c0c3b22c8e3b79c12b71781d48a12
.email.com <gangisettymani98@gmail.com>
2026-09-07 12:22:14 +0530

    update

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "components" {
     for_each = var.components
-    source = "git ::https://github.com/gangisettymanikumar79-oss/terraform-roboshop-component.git?ref=main"
+    source = "git::https://github.com/gangisettymanikumar79-oss/terraform-roboshop-component.git?ref=main"
     # source = "git::https://github.com/daws-90s/terraform-roboshop-component.git?ref=main"
     environment = var.environment
     component = each.key
