5738db5eb7c9753852a6bd6a07a6d81ae8f95bf9
.email.com <gangisettymani98@gmail.com>
2026-09-08 19:03:59 +0530

    terraform

--- 90-components/bootstrap.sh
+++ 90-components/bootstrap.sh
@@ -10,8 +10,8 @@ chmod -R 755 /var/log/roboshop
 touch /var/log/roboshop/ansible.log
 
 cd /home/ec2-user
-git clone https://github.com/gangisettymanikumar79-oss/roboshop-ansible-V3.git roboshop-ansible-v3
-cd /home/ec2-user/roboshop-ansible-v3
+git clone https://github.com/gangisettymanikumar79-oss/roboshop-ansible-V3.git 
+cd roboshop-ansible-v3
 git pull
 ansible-playbook -e component=$component -e env=$environment -e app_version=$app_version roboshop.yaml
 
