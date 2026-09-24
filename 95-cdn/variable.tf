e0066427b3b9f116eec7746c7707f06ee0ab3509
.email.com <gangisettymani98@gmail.com>
2026-09-24 18:07:15 +0530

    if i want create another terraform file

---
+++ 95-cdn/variable.tf
@@ -0,0 +1,15 @@
+variable "environment" {
+    default = "dev"
+}
+
+variable "project" {
+    default = "roboshop"
+}
+
+variable "zone_id" {
+    default = "Z08857072UVZ2NYWVCS7E"
+}  
+
+variable "domain_name" {
+    default = "manikumar.online"
+}
